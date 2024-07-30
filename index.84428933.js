9371a2f0179a7c9567696b27b643fd8adac914f8
LadyLiza <elizabet132010@gmail.com>
2024-07-30 20:03:21 +0200

    make build

---
+++ index.84428933.js
@@ -0,0 +1,2 @@
+var t=document.querySelector(".wall"),e=document.querySelector(".spider");e.style.position="absolute",e.style.top="".concat(t.clientHeight/2-e.clientHeight/2,"px"),e.style.left="".concat(t.clientWidth/2-e.clientWidth/2,"px");
+//# sourceMappingURL=index.84428933.js.map
